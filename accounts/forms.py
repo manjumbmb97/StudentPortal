bd483f3132df0adcba989e6aa4e1220f52ae33c3
Manjunath Maigur <manjumbmb97@gmail.com>
2018-07-11 20:47:24 +0530

    unique email

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,10 +9,6 @@ class UserForm(UserCreationForm):
 		fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2',]
 
 	def clean_email(self):
-        """
-        Validate that the supplied email address is unique for the
-        site.
-        """
 		if User.objects.filter(email__iexact=self.cleaned_data['email']):
 			raise forms.ValidationError("Email id already exists")
 		return self.cleaned_data['email']
